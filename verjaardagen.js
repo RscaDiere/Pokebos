84e5075c1513ece5a0bca30cc56787ba789b4534
RscaDiere <deman_dieter@hotmail.com>
2026-01-17 14:50:18 +0100

    Update verjaardagen.js

--- verjaardagen.js
+++ verjaardagen.js
@@ -1,11 +1,10 @@
 // { naam: "Naam", dag: DD, maand: MM }
 // Voeg hier nieuwe verjaardagen toe
 const verjaardagen = [
-  { naam: "Jan", dag: 3, maand: 3 },
-  { naam: "Piet", dag: 17, maand: 6 },
-  { naam: "Lisa", dag: 22, maand: 10 },
-  { naam: "Alex", dag: 5, maand: 1 },
-  { naam: "Sofie", dag: 14, maand: 8 }
+  { naam: "Gina", dag: 11, maand: 1 },
+  { naam: "Dieter", dag: 4, maand: 3 },
+  { naam: "Sigurd", dag: 18, maand: 9 },
+  { naam: "Jeroen", dag: 16, maand: 12 }
 ];
 
 function toonKomendeVerjaardagen() {
